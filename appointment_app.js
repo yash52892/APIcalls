18ee7e70d27c4f25998169d020714a73d305dd11
yash52892 <97838432+yash52892@users.noreply.github.com>
2023-07-07 11:01:34 +0530

    Add files via upload

---
+++ appointment_app.js
@@ -0,0 +1,17 @@
+function add_to_local(event){
+    event.preventDefault();
+const name=event.target.username.value;
+const email=event.target.email.value;
+const phone=event.target.phonenumber.value;
+const obj={
+    name, email, phone
+};
+localStorage.setItem(obj.email, JSON.stringify(obj))
+showusers(obj);
+}
+function showusers(obj){
+const parent=document.getElementById("list_of_users")
+const child=document.createElement('li');
+child.textContent=obj.name+'-'+obj.email+'-'+obj.phone;
+parent.appendChild(child)
+}
