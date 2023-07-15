92aab822a76bbc2ee141343faedf048b865c25af
yash52892 <97838432+yash52892@users.noreply.github.com>
2023-07-15 10:54:12 +0530

    Update appointment_app.js

--- appointment_app.js
+++ appointment_app.js
@@ -1,9 +1,4 @@
-window.addEventListener("DOMContentLoaded",()=>{
-    axios.get("https://crudcrud.com/api/ad184ad3e49243ba9c50a53d8a3f3d6f/appointment")
-    .then((res)=>{
-        for(let i=0;i<res.data.length;i++)
-            showusers(res.data[i])});
-    })
+  
 function add_to_local(event){
         event.preventDefault();
     const name=event.target.username.value;
@@ -12,14 +7,57 @@ function add_to_local(event){
     const obj={
         name, email, phone
     };
-    
-
-    axios.post("https://crudcrud.com/api/ad184ad3e49243ba9c50a53d8a3f3d6f/appointment", obj)
-    .then((res)=>showusers(res.data));
+    axios.post("https://crudcrud.com/api/454e2c5888b248dfaadebc4ebfe82e88/appointment", obj)
+    .then((res)=>showusersonscreen(res.data))
+    .catch((rej)=>{
+        console.log("Somethig went wrong: post");
+    })
 }
-function showusers(obj){
+
+function showusersonscreen(obj){
     const parent=document.getElementById("list_of_users")
+
     const child=document.createElement('li');
-    child.textContent=obj.name+'-'+obj.email+'-'+obj.phone;
+    child.textContent=obj._id+'-'+obj.name+'-'+obj.email+'-'+obj.phone+"---";
+
+    const del=document.createElement('input');
+    del.type="button"
+    del.value='Delete'
+    del.onclick = function (){
+        let x="https://crudcrud.com/api/454e2c5888b248dfaadebc4ebfe82e88/appointment/"+obj._id;
+        axios.delete(x)
+        .then((res)=>{
+            parent.removeChild(child);
+        })
+        .catch((rej)=>{
+            console.log("Somethig went wrong: Delete");
+        })
+        
+    }
+    child.appendChild(del);
     parent.appendChild(child)
 }
+
+function showusers(obj){
+    axios.get("https://crudcrud.com/api/454e2c5888b248dfaadebc4ebfe82e88/appointment")
+    .then((res)=>{
+        for(let i=0;i<res.data.length;i++)
+        showusersonscreen(res.data[i])
+        })
+        .catch((rej)=>{
+            console.log("Somethig went wrong: Retrive");
+        })
+     
+}
+
+function deleteuserfromscreen(id){
+    const parentNode=document.getElementById("list_of_users");
+    const childToBeDeleted=document.getElementById("obj._id");
+    if(childToBeDeleted){
+        parentNode.removeChild(childToBeDeleted);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", ()=>{
+    showusers();
+})
