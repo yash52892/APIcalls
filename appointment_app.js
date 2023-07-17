e8ffc4b2bd0e56aeab670a29628793be0912ac95
yash52892 <97838432+yash52892@users.noreply.github.com>
2023-07-17 11:50:23 +0530

    Update appointment_app.js

--- appointment_app.js
+++ appointment_app.js
@@ -7,7 +7,7 @@ function add_to_local(event){
     const obj={
         name, email, phone
     };
-    axios.post("https://crudcrud.com/api/454e2c5888b248dfaadebc4ebfe82e88/appointment", obj)
+    axios.post("https://crudcrud.com/api/e594dbdc823042b1ae255dfb90849b19/appointment", obj)
     .then((res)=>showusersonscreen(res.data))
     .catch((rej)=>{
         console.log("Somethig went wrong: post");
@@ -16,30 +16,45 @@ function add_to_local(event){
 
 function showusersonscreen(obj){
     const parent=document.getElementById("list_of_users")
-
     const child=document.createElement('li');
     child.textContent=obj._id+'-'+obj.name+'-'+obj.email+'-'+obj.phone+"---";
-
     const del=document.createElement('input');
     del.type="button"
     del.value='Delete'
-    del.onclick = function (){
-        let x="https://crudcrud.com/api/454e2c5888b248dfaadebc4ebfe82e88/appointment/"+obj._id;
-        axios.delete(x)
-        .then((res)=>{
-            parent.removeChild(child);
-        })
-        .catch((rej)=>{
-            console.log("Somethig went wrong: Delete");
-        })
-        
+    del.onclick= function (){
+        let x="https://crudcrud.com/api/e594dbdc823042b1ae255dfb90849b19/appointment/"+obj._id;
+    axios.delete(x)
+    .then((res)=>{
+        parent.removeChild(child);
+    })
+    .catch((rej)=>{
+        console.log("Somethig went wrong: Delete");
+    })
+    };
+    const edit=document.createElement('input');
+    edit.type="button"
+    edit.value='Edit'
+    
+    edit.onclick = function (){
+        document.getElementById("n").value=obj.name;
+        document.getElementById("e").value=obj.email;
+        document.getElementById("p").value=obj.phone;
+        let x='https://crudcrud.com/api/e594dbdc823042b1ae255dfb90849b19/appointment/'+obj._id;
+    axios.delete(x)
+    .then((res)=>{
+        parent.removeChild(child);
+    })
+    .catch((rej)=>{
+        console.log("Somethig went wrong: Delete");
+    })
     }
+    child.appendChild(edit);
     child.appendChild(del);
-    parent.appendChild(child)
+    parent.appendChild(child);  
 }
 
 function showusers(obj){
-    axios.get("https://crudcrud.com/api/454e2c5888b248dfaadebc4ebfe82e88/appointment")
+    axios.get("https://crudcrud.com/api/e594dbdc823042b1ae255dfb90849b19/appointment")
     .then((res)=>{
         for(let i=0;i<res.data.length;i++)
         showusersonscreen(res.data[i])
@@ -47,7 +62,6 @@ function showusers(obj){
         .catch((rej)=>{
             console.log("Somethig went wrong: Retrive");
         })
-     
 }
 
 function deleteuserfromscreen(id){
